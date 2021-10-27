23ab1ff3fafd696531bf420b3c1383facf804bad
Hiroki Shimada <taiger0421@yahoo.co.jp>
2021-10-27 13:22:50 +0900

    removed

--- dev_ros_nav/melodic/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --name test -it -v ~/prog/waypoint_nav:/home/ubuntu/catkin_ws/src/waypoint_nav -v ~/share:/home/ubuntu/share -e DISPLAY=$DISPLAY_MAC -w /home/ubuntu ros:melodic bash
